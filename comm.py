4bad0ed268d41948dc3e73f1c17f8bc3354d25ca
fireballff <106222447+fireballff@users.noreply.github.com>
2024-10-08 15:09:18 +0800

    Create comm.py
    Needs to be fixed. Communication protoccol for mac

---
+++ comm.py
@@ -0,0 +1,27 @@
+import socket
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.bind(('pi3.local', 1))  # Listen on all available interfaces
+    client_socket.listen(1)
+    print("Waiting for Raspberry Pi to connect...")
+
+    conn, addr = client_socket.accept()
+    print(f"Connected to {addr}")
+
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            received_message = data.decode()
+            first_name, class_name, times_called = received_message.split(',')
+            print(f"Name: {first_name}, Class: {class_name}, Times Called: {times_called}")
+
+    finally:
+        conn.close()
+        client_socket.close()
+
+start_client()
+
+

85c2530ebcff364b86a65ad75adab6b0a30166c6
Shreyas0988 <145433465+Shreyas0988@users.noreply.github.com>
2024-10-16 15:41:12 +0800

    feat: added communication between GUI and raspberry

--- comm.py
+++ comm.py
@@ -1,25 +1,171 @@
+import RPi.GPIO as GPIO
+import random
+from time import sleep
+from RPLCD.i2c import CharLCD
 import socket
 
-def start_client():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.bind(('0.0.0.0',5001))  # Listen on all available interfaces
-    client_socket.listen(1)
-    print("Waiting for Raspberry Pi to connect...")
+# Define students for all classes
+students = {
+    "Class A2": [
+        "Dimmock, Claire", "Gupta, Vardaan", "Hardcastle, Auric", "Jia, Irene", "Jiao, Gabriel",
+        "Jung, Eli", "Lau, KJ", "Lee, Jenna", "Lingnau, Fritzy", "Moorjani, Noori",
+        "Park, Sangweon", "Parpia, Jam", "Peng, Claire", "Rawat, Keshav", "Saadullah, Zakaria",
+        "Thoman, Aaron", "Wang, Justin",
+    ],
+    "Class A3": [
+        "Bhatia, Myra", "Brown, Simon", "Burnett, Ben", "Coppell, Claire", "Kim, Selin",
+        "Li, Sophie", "Lin, Cindy", "McFadzen, Lily", "Metz, Jaci", "Narula, Shaurya",
+        "Rosenkilde, Matilda", "Terry, Ella", "Tremarco, Nola", "Troija, Isabelle",
+        "van Eldik, Felix", "Wheeler, Talia",
+    ],
+    "Class B1": [
+        "Chopra, Diya", "Chopra, Shreyas", "Craig, Rohan", "Duran, James", "Gu, Jiawei",
+        "Kim, Myoungjoon", "Landon, Colin", "Lee, Sumin", "Lin, HaiCheng Walter",
+        "Nahata, Priyam", "Park, Lyle", "Sikhakollu, Phani", "Singh, Jasmeh",
+        "Singh Kohli, Rysa", "Thompson, Austin", "Wang, Weihao",
+    ],
+    "Class B2": [
+        "Anvekar, Mannat", "Bakhshi, Kareena", "Deshwal, Veehaan", "Dulat, Lucas",
+        "Hopkins, Matthew", "Hsiao, Yue Jhen", "Jermyn, Anna Mae", "Kim, Andrew",
+        "Kim, Isaac", "Lee, Kevin(Jeongwoo)Lee", "Li, Xingyue", "Liu, Minna", "Ma, Grace",
+        "Oravec, Haven", "Sethi, Rohaan", "Steckler, Billy", "Vyas, Sara", "Welker, Layton",
+    ],
+    "Class B4": [
+        "Aggarwal, Avni", "Arnold, Aidan", "Bai, Ken", "Bohra, Mia", "Chen, Adrian",
+        "Chen, Melinda", "Chi, Amber", "Choi, Kaitlyn", "Chopra, Navya", "Fung, Isabella",
+        "Kim, Yijoon", "Laroia, Leela", "Li, Changan", "Mathur, Azara", "Nakahara, Mirei",
+        "Shih, Chantelle", "Sun, Kolb", "Yang, Amy", "Yeh, Matthew", "Yoon, Jisoo",
+    ],
+}
 
-    conn, addr = client_socket.accept()
-    print(f"Connected to {addr}")
+BUTTON_PIN_1 = 21
+BUTTON_PIN_2 = 5
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_PIN_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    try:
+lcd = CharLCD('PCF8574', 0x3f)
+names = {student: 0 for class_students in students.values() for student in class_students}
+selected_class = list(students.keys())[0]
+current_class_index = 0
+
+# Function to set up server and listen for connections
+def setup_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow reuse of the socket
+    server_socket.bind(('0.0.0.0', 12345))  # Bind to all available interfaces
+    server_socket.listen(5)  # Listen for up to 5 incoming connections
+    return server_socket
+
+server_socket = setup_server()
+client_socket = None
+
+def wait_for_client():
+    global client_socket
+    while client_socket is None:
+        try:
+            print("Waiting for connection...")
+            client_socket, client_address = server_socket.accept()
+            print(f"Connected to {client_address}")
+        except Exception as e:
+            print(f"Error accepting connection: {e}")
+            client_socket = None
+            sleep(1)  # Retry delay
+
+wait_for_client()
+
+# Function to wait for button release
+def wait_for_release(button_pin):
+    while GPIO.input(button_pin) == GPIO.LOW:
+        sleep(0.01)
+
+# Function to choose a name based on weighted calls
+def choose_name():
+    total_calls = sum(names.values())
+    weights = [1 / (1 + count) for count in names.values()]
+    chosen_name = random.choices(list(names.keys()), weights=weights, k=1)[0]
+    names[chosen_name] += 1
+    return chosen_name
+
+# Function to extract the first name
+def extract_first_name(full_name):
+    return full_name.split(", ")[1]
+
+# Function to display on LCD
+def display_on_lcd(line1, line2=""):
+    lcd.clear()
+    lcd.write_string(line1)
+    if line2:
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string(line2)
+
+try:
+    while True:
+        display_on_lcd("Select Class")
+        
         while True:
-            data = conn.recv(1024)
-            if not data:
+            if GPIO.input(BUTTON_PIN_1) == GPIO.LOW:
+                wait_for_release(BUTTON_PIN_1)
+                current_class_index = (current_class_index + 1) % len(students)
+                selected_class = list(students.keys())[current_class_index]
+                display_on_lcd(f"{selected_class} Selected")
+
+            if GPIO.input(BUTTON_PIN_2) == GPIO.LOW:
+                wait_for_release(BUTTON_PIN_2)
+                display_on_lcd(f"Class {selected_class}", "Confirmed")
+                sleep(1)
                 break
-            received_message = data.decode()
-            first_name, class_name, times_called = received_message.split(',')
-            print(f"Name: {first_name}, Class: {class_name}, Times Called: {times_called}")
 
-    finally:
-        conn.close()
+        display_on_lcd(f"Class {selected_class}", "Pick Name")
+
+        while True:
+            try:
+                client_socket.settimeout(0.1)
+                try:
+                    data = client_socket.recv(1024).decode()
+                    if data:
+                        print(f"Received: {data}")
+                        if len(data.split(',')) == 3:
+                            first_name, class_name, times_called = data.split(',')
+                            display_on_lcd(f"Name: {first_name}", f"Called: {times_called}")
+                        else:
+                            print("Data format incorrect")
+                    else:
+                        print("No data received.")
+                except socket.timeout:
+                    pass
+                except Exception as e:
+                    print(f"Error receiving data: {e}")
+
+            except socket.timeout:
+                pass
+
+            if GPIO.input(BUTTON_PIN_1) == GPIO.LOW:
+                wait_for_release(BUTTON_PIN_1)
+                chosen_name = choose_name()
+                first_name = extract_first_name(chosen_name)
+                message = f"{first_name},{selected_class},{names[chosen_name]}"
+                display_on_lcd(f"Name: {first_name}", f"Called: {names[chosen_name]}")
+
+                try:
+                    client_socket.sendall(message.encode(1024))
+                except BrokenPipeError:
+                    print("Client disconnected, trying to reconnect...")
+                    client_socket.close()
+                    client_socket = None
+                    wait_for_client()
+                except Exception as e:
+                    print(f"Error sending data to GUI: {e}")
+
+            if GPIO.input(BUTTON_PIN_2) == GPIO.LOW:
+                wait_for_release(BUTTON_PIN_2)
+                display_on_lcd("Goodbye")
+                sleep(2)
+                names = {student: 0 for class_students in students.values() for student in class_students}
+                break
+
+finally:
+    if client_socket:
         client_socket.close()
-
-start_client()
+    server_socket.close()
+    GPIO.cleanup()
